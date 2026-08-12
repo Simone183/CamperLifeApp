6e23937bf00427651355f8272f8f608c955c38a3
Simone183 <84089716+Simone183@users.noreply.github.com>
2026-08-12 20:09:17 +0200

    New update

--- android/app/src/main/java/com/ViaCamper/myapp/MainActivity.java
+++ android/app/src/main/java/com/ViaCamper/myapp/MainActivity.java
@@ -1,5 +1,19 @@
 package com.ViaCamper.myapp;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            if (FirebaseApp.getApps(this).isEmpty()) {
+                FirebaseApp.initializeApp(this);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
