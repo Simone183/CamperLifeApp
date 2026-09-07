d1a45fc79434fc51c0f148e7708784ddc797411d
Simone183 <84089716+Simone183@users.noreply.github.com>
2026-09-07 20:51:46 +0200

    refactor: remove manual firebase initialization
    Clean up MainActivity by removing the hardcoded programmatic Firebase initialization in favor of standard configuration via google-services.json.

--- android/app/src/main/java/com/ViaCamper/myapp/MainActivity.java
+++ android/app/src/main/java/com/ViaCamper/myapp/MainActivity.java
@@ -1,26 +1,5 @@
 package com.ViaCamper.myapp;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        try {
-            if (FirebaseApp.getApps(this).isEmpty()) {
-                FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setApplicationId("1:17441453721:android:b0f4028724ea2bb276aa08")
-                    .setProjectId("calm-light-fg02f")
-                    .setApiKey("AIzaSyBrLUDywyD1lgs6WyS1fd6dvegBjExJxTM")
-                    .setGcmSenderId("17441453721")
-                    .build();
-                FirebaseApp.initializeApp(this, options);
-            }
-        } catch (Exception e) {
-            android.util.Log.e("ViaCamper", "Firebase programmatic init exception", e);
-        }
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
